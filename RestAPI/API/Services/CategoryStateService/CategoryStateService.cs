8aaafc351c3ca9c66567d2a5237bb1f8025796f1
Andres Ramirez <ramirezsilva,felipe.silva@gmail.com>
2024-01-24 14:26:55 -0600

    CategoryState Service
    Added CategoryState Service and Interface

---
+++ RestAPI/API/Services/CategoryStateService/CategoryStateService.cs
@@ -0,0 +1,18 @@
+﻿using API.Data;
+using API.Data.Models;
+namespace API.Services
+{
+    public class CategoryStateService : ICategoryState
+    {
+        private readonly StoreDB _database;
+        public CategoryStateService(StoreDB database)
+        {
+            this._database = database;
+        }
+        public IQueryable<CategoryState> ListCategoryStates()
+        {
+            return this._database
+                    .CategoryState;
+        }
+    }
+}
