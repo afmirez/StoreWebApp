4d68cf5888bcbb9731b584e528f0a9fd13a69347
Andres Ramirez <ramirezsilva,felipe.silva@gmail.com>
2024-01-25 08:22:35 -0600

    Added Product Service and Interface
    Added Product Service and Interface

---
+++ RestAPI/API/Services/ProductService/ProductService.cs
@@ -0,0 +1,49 @@
+﻿using API.Data;
+using API.Data.Models;
+using Microsoft.EntityFrameworkCore;
+namespace API.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly StoreDB _database;
+        public ProductService(StoreDB database)
+        {
+            this._database = database;
+        }
+        public IQueryable<Product> ListProducts()
+        {
+            return this._database
+                    .Product
+                    .Include(p => p.Category)
+                    .Include(p => p.ProductState);
+        }
+        public async Task<Product?> FindProduct(int id)
+        {
+            return await this._database
+                    .Product
+                    .Include(p => p.Category)
+                    .Include(p => p.ProductState)
+                    .Where(p => p.Id == id)
+                    .FirstOrDefaultAsync();
+        }
+        public async Task InsertProduct(Product entity)
+        {
+            this._database.Product.Add(entity);
+            await this._database.SaveChangesAsync();
+            await this._database.Entry(entity).Reference(p => p.Category).LoadAsync();
+            await this._database.Entry(entity).Reference(p => p.ProductState).LoadAsync();
+        }
+        public async Task UpdateProduct(Product entity)
+        {
+            this._database.Product.Update(entity);
+            await this._database.SaveChangesAsync();
+            await this._database.Entry(entity).Reference(p => p.Category).LoadAsync();
+            await this._database.Entry(entity).Reference(p => p.ProductState).LoadAsync();
+        }
+        public async Task DeleteProduct(Product entity)
+        {
+            this._database.Product.Remove(entity);
+            await this._database.SaveChangesAsync();
+        }
+    }
+}
