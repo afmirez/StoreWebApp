9dde9fab6a2ef712b7635c0cd94a90b571ef15bc
Andres Ramirez <ramirezsilva,felipe.silva@gmail.com>
2024-02-01 13:40:55 -0600

    s
    sd

--- RestAPI/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ RestAPI/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b75045f312199be101adfbd275bd96ee701e47a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
