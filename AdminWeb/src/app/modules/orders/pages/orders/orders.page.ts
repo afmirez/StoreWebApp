7758a9c027be2139646cb22736412d049fc4c93a
edrodriguez92 <ed_rodriguez92@outlook.com>
2024-02-12 15:44:50 -0600

    edrodriguez92-Frontend

---
+++ AdminWeb/src/app/modules/orders/pages/orders/orders.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.page.html',
+  styleUrls: ['./orders.page.css']
+})
+export class OrdersPage {
+
+}
